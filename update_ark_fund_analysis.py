23b219ce196487e3289e6d0551c3ce2523c3b4c5
a-dpq <anthony@depasquale.org>
2021-07-12 10:41:24 +0200

    Scripts for updating website

--- update_ark_fund_analysis.py
+++ update_ark_fund_analysis.py
@@ -1,12 +1,3 @@
-'''
-Run `crontab -e` and add the following (with correct paths) to check for new data every 15 minutes (starting at minute 2) and export the notebook, and update the website when new data is downloaded.
-PYTHON_PATH=/home/ubuntu/miniconda3/bin/python
-ARK_FUND_ANALYSIS=/home/ubuntu/ark_fund_analysis
-WEBSITE_REPO=/home/ubuntu/a-dpq.github.io
-COMMAND="cd $ARK_FUND_ANALYSIS && $PYTHON_PATH cron_export_notebook.py && cd $WEBSITE_REPO && $PYTHON_PATH update_ark_fund_analysis.py"
-2-59/15 * * * * (eval $COMMAND) 2>> $ARK_FUND_ANALYSIS/output/cron_stderr.log 1> $ARK_FUND_ANALYSIS/output/cron_stdout.log
-'''
-
 import pickle
 from pathlib import Path
 from datetime import datetime
@@ -19,40 +10,32 @@ time_format = '%Y-%m-%d %H:%M:%S %z'
 base_dir = Path(__file__).parent.absolute()
 exported_notebook_path = base_dir / '../ark_fund_analysis/output/ark_fund_analysis.html'
 
+# Check when notebook was last exported
 if os.path.isfile(exported_notebook_path):
-    notebook_updated = datetime.fromtimestamp(os.path.getmtime(exported_notebook_path)).replace(tzinfo=utc).strftime(time_format)
+    notebook_exported = datetime.fromtimestamp(os.path.getmtime(exported_notebook_path)).replace(tzinfo=utc)
 else:
-    notebook_updated = None
+    notebook_exported = None
 
-notebook_previously_updated_path = base_dir / 'notebook_previously_updated.p'
+website_updated_path = base_dir / 'ignore/website_updated.p'
 
-if os.path.isfile(notebook_previously_updated_path):
-    with open(notebook_previously_updated_path, 'rb') as handle:
-        notebook_previously_updated = pickle.load(handle)
+# Make `ignore` directory if it doesn't exist
+if not os.path.exists(os.path.dirname(website_updated_path)):
+    os.makedirs(os.path.dirname(website_updated_path))
+
+# Check when website was last updated
+if os.path.isfile(website_updated_path):
+    with open(website_updated_path, 'rb') as handle:
+        website_updated = pickle.load(handle)
 else:
-    notebook_previously_updated = None
+    website_updated = None
 
-# !! Check if previous updated time has been saved, and if so, compare to notebook_updated
-# !! If notebook_updated > previous saved time, or no saved time is found, run the following:
-
-print(f'Notebook updated: {notebook_updated}')
-print(f'Notebook previously updated: {notebook_previously_updated}')
-
-if notebook_previously_updated is None or notebook_updated > notebook_previously_updated:
+if notebook_exported is None or website_updated is None or notebook_exported > website_updated:
     git_path = '/usr/bin/git'
     website_repo = Path('/home/ubuntu/a-dpq.github.io/')
     website_root = website_repo / 'docs'
     notebook_dest_path = website_root / 'ark-fund-analysis/index.html'
     timestamp = datetime.now().astimezone(utc).strftime(time_format) # Equivalent shell command: date -u +"%Y-%m-%d %H:%M:%S %z"
-
-    command = f"cd {website_repo} && {git_path} pull && cp '{exported_notebook_path}' '{notebook_dest_path}' && {git_path} add -A && {git_path} commit -m 'Update {timestamp}' && {git_path} push"
-
-    # try:
-    #     subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-    # except subprocess.CalledProcessError as e:
-    #     raise RuntimeError(f"Command '{e.cmd}' returned with error (code {e.returncode}): {e.output}")
-
+    command = f"cd {website_repo} && {git_path} pull && cp '{exported_notebook_path}' '{notebook_dest_path}' && {git_path} add -A && {git_path} commit -m 'Update ARK Fund Analysis ({timestamp})' && {git_path} push"
     subprocess.run(command, shell=True, check=True)
-
-    with open(notebook_previously_updated_path, 'wb') as handle:
-        pickle.dump(notebook_updated, handle)
+    with open(website_updated_path, 'wb') as handle:
+        pickle.dump(notebook_exported, handle)
---
+++ update_ark_fund_analysis.sh
@@ -0,0 +1,20 @@
+: '
+Crontab entry:
+
+# Every 15 minutes (starting at minute 2)
+ARK_FUND_ANALYSIS=/home/ubuntu/ark_fund_analysis
+WEBSITE_REPO=/home/ubuntu/a-dpq.github.io
+2-59/15 * * * * (/usr/bin/bash $WEBSITE_REPO/update_ark_fund_analysis.sh) 2> $ARK_FUND_ANALYSIS/output/cron_stderr.log 1> $ARK_FUND_ANALYSIS/output/cron_stdout.log
+'
+
+PYTHON_PATH=/home/ubuntu/miniconda3/bin/python
+GIT_PATH=/usr/bin/git
+ARK_FUND_ANALYSIS=/home/ubuntu/ark_fund_analysis
+WEBSITE_REPO=/home/ubuntu/a-dpq.github.io
+
+# Check for new data and export the notebook
+cd $ARK_FUND_ANALYSIS && $PYTHON_PATH cron_export_notebook.py
+# Upload exported notebook to website
+cd $WEBSITE_REPO && $PYTHON_PATH update_ark_fund_analysis.py
+# Update data repository and upload
+cd $ARK_FUND_ANALYSIS/data/ark_fund_holdings && $GIT_PATH pull && $GIT_PATH add -A && $GIT_PATH commit -m 'Add new data from server' && $GIT_PATH push
